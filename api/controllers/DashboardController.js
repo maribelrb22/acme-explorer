c633c9e0e17d3eacd98fc63ea2e65bd4a42a9260
José María <josemariacruzlorite@gmail.com>
2023-01-31 20:56:36 +0100

    Fix get to return latest dashboard

--- api/controllers/DashboardController.js
+++ api/controllers/DashboardController.js
@@ -4,7 +4,7 @@ import DashboardSchema from '../models/DashboardModel.js'
 
 const getDashboard = async (req, res) => {
     try {
-        const dashboard = await getDashboard.find({})
+        const dashboard = await DashboardSchema.find({}).sort({createdAt: -1}).limit(1);
         res.status(200).json(dashboard)
     } catch (err) {
         res.status(500).json(err)
