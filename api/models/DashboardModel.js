decec3898443523724afc4452ce4e53d516c7ac7
José María <josemariacruzlorite@gmail.com>
2023-01-31 20:42:50 +0100

    Create Dashboard schema

---
+++ api/models/DashboardModel.js
@@ -0,0 +1,34 @@
+'use strict'
+
+import mongoose from 'mongoose'
+
+
+const DashboardSchema = new mongoose.Schema({
+    tripsPerManagerStats: {
+        type: StatsSchema,
+        required: 'Enter trips per manager stats'
+    },
+    applicationsPerTripStats: {
+        type: StatsSchema,
+        required: 'Enter applications per trip stats'
+    },
+    tripPriceStats: {
+        type: StatsSchema,
+        required: 'Enter trip price stats'
+    },
+    statusRatios: {
+        type: {
+            PENDING:    { type: String, required: 'Enter PENDING applications ratio' },
+            REJECTED:   { type: String, required: 'Enter REJECTED applications ratio' },
+            DUE:        { type: String, required: 'Enter DUE applications ratio' },
+            ACCEPTED:   { type: String, required: 'Enter ACCEPTED applications ratio'},
+            CANCELLED:  { type: String, required: 'Enter CANCELLED applications ratio' }
+        },
+        required: 'Enter application status ratios'
+    }
+}, {strict: false});
+
+const model = mongoose.model('Dashboard', TripSchema);
+
+export const schema = model.schema;
+export default model;
