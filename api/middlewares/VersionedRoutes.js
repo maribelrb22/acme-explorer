63475468b9e83f34fd541c55311c3303db75650c
José María <josemariacruzlorite@gmail.com>
2023-02-11 13:45:33 +0100

    Add [ ] to error message

--- api/middlewares/VersionedRoutes.js
+++ api/middlewares/VersionedRoutes.js
@@ -5,7 +5,7 @@ function versionedRoutes(versions) {
         const apiVersion = req.header('X-API-Version');
 
         if (!versions.includes(apiVersion)) {   
-            return res.status(400).send({ error: `X-API-Version must be a value in: ${versions}` });
+            return res.status(400).send({ error: `X-API-Version must be a value in: [${versions}]` });
         } 
         
         req.url = `/${apiVersion}${req.url}`;
