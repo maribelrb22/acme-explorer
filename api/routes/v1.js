7b23584a4ecf1dbc19c5598f843fbc0dc10aae80
José María <josemariacruzlorite@gmail.com>
2023-02-11 16:02:45 +0100

    Add /finders to v1 router

--- api/routes/v1.js
+++ api/routes/v1.js
@@ -7,6 +7,7 @@ import { tripsV1 } from './TripRoutes.js'
 import { dashboardsV1 } from './DashboardRoutes.js'
 import { sponsorshipsV1 } from './SponsorshipRoutes.js'
 import { flatRateConfigurationsV1 } from './FlatRateConfigurationRoutes.js'
+import { findersV1 } from './FinderRoutes.js'
 
 const v1 = express.Router();
 
@@ -16,5 +17,6 @@ v1.use('/trips', tripsV1)
 v1.use('/dashboards', dashboardsV1)
 v1.use('/sponsorships', sponsorshipsV1)
 v1.use('/flat-rate', flatRateConfigurationsV1)
+v1.use('/finders', findersV1)
 
 export default v1;
