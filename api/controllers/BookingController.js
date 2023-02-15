89937e84a30bd7faf07c291ee142eba8a8f7d57a
José María <josemariacruzlorite@gmail.com>
2023-02-15 15:50:05 +0100

    Remove Booking duplicated validation

--- api/controllers/BookingController.js
+++ api/controllers/BookingController.js
@@ -16,22 +16,9 @@ const postBooking = async (req, res, next) => {
     req.body.moment = undefined
     req.body.status = undefined
     try {
-        // verify we can book this trip
-        const trip = await TripModel.findById(req.params.trip)
-        if (!trip) {
-            res.status(404).json({message: "Trip not found"})
-        } else if (trip.cancel) {
-            res.status(400).json({message: "Cannot book a cancelled trip"})
-        } else if (trip.published) {
-            res.status(400).json({message: "Cannot book a unpublished trip"})
-        } else if (trip.startDate < new Date()) {
-            res.status(400).json({message: "Cannot book a trip that has already started"})
-        } else {
-            // once the trip is verified, we can create the booking
-            const booking = new BookingModel(req.body)
-            await booking.save()
-            res.status(201).json(booking)
-        }
+        const booking = new BookingModel(req.body)
+        await booking.save()
+        res.status(201).json(booking)
     } catch (err) {
         req.err = err;
         next()
