e8e88a099d3322a483386bc9cb1a39c64d9f9ee1
José María <josemariacruzlorite@gmail.com>
2023-02-21 17:41:09 +0100

    Add cache params to configuration

--- api/models/ConfigurationModel.js
+++ api/models/ConfigurationModel.js
@@ -8,7 +8,20 @@ const ConfigurationSchema = new mongoose.Schema({
         min: 0,
         required: 'Enter the flat rate of the configuration',
     },
-
+    finderCacheSeconds: {
+        type: Number,
+        default: 60 * 60,
+        min: 60,
+        max: 60 * 60 * 24,
+        required: 'Enter the finder cache time of the configuration',
+    },
+    finderSearchLimit: {
+        type: Number,
+        default: 10,
+        min: 0,
+        max: 100,
+        required: 'Enter the finder search limit of the configuration',
+    },
 }, { strict: false });
 
 const model = mongoose.model('Configuration', ConfigurationSchema);
