6306e44a57ea2eb60fe20fc042bbd2657fa2e8d5
José María <josemariacruzlorite@gmail.com>
2023-02-21 17:38:45 +0100

    Add searchTrip to finder controller

--- api/controllers/FinderController.js
+++ api/controllers/FinderController.js
@@ -1,6 +1,10 @@
 'use strict'
 
+import mongoose from 'mongoose';
+
+import ConfigurationModel from '../models/ConfigurationModel.js';
 import FinderModel from '../models/FinderModel.js';
+import { searchTrips as _searchTrips } from '../services/TripSearcherService.js';
 
 // Finder CRUD
 const createFinder = async (req, res, next) => {
@@ -16,7 +20,7 @@ const createFinder = async (req, res, next) => {
 
 const getFinder = async (req, res, next) => {
     try {
-        const finder = await FinderModel.findById(req.params.explorerId);
+        const finder = await FinderModel.findById({ explorerId: req.params.explorerId });
         if (finder) {
             res.status(200).json(finder);
         } else {
@@ -30,7 +34,7 @@ const getFinder = async (req, res, next) => {
 
 const updateFinder = async (req, res, next) => {
     try {
-        const finder = await FinderModel.findByIdAndUpdate(req.params.explorerId, req.body, { new: true });
+        const finder = await FinderModel.findByIdAndUpdate({ explorerId: req.params.explorerId }, req.body, { new: true });
         if (finder) {
             res.status(200).json(finder);
         } else {
@@ -44,7 +48,7 @@ const updateFinder = async (req, res, next) => {
 
 const deleteFinder = async (req, res, next) => {
     try {
-        const finder = await FinderModel.findByIdAndDelete(req.params.explorerId);
+        const finder = await FinderModel.findByIdAndDelete({ explorerID: req.params.explorerId });
         if (finder) {
             res.status(200).json(finder);
         } else {
@@ -56,19 +60,26 @@ const deleteFinder = async (req, res, next) => {
     }
 }
 
-const searchTripsWithFinder = async (req, res, next) => {
+const searchTrips = async (req, res, next) => {
     try {
-        const finder = await FinderModel.findById(req.params.explorerId);
-        if (finder) {
-            const trips = await finder.searchTrips();
-            res.status(200).json(trips);
-        } else {
-            res.status(404).json({ message: 'Finder not found' });
-        }
+        // get finder cache time from configuration
+        const configuration = await ConfigurationModel.find()
+        const finderSearchLimit = configuration.finderSearchLimit
+
+        // get the explorer finder
+        const finder = await FinderModel.findOne({explorer: req.params.explorerId});
+
+        const trips = await _searchTrips(
+            finder.keyword,
+            0,
+            1,
+            undefined,
+            undefined)
+        res.status(200).json(trips);
     } catch (err) {
         req.err = err;
         next()
     }
 }
 
-export { createFinder, getFinder, updateFinder, deleteFinder, searchTripsWithFinder };
+export { createFinder, getFinder, updateFinder, deleteFinder, searchTrips};
